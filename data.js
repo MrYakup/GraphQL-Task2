6ef67ffdd6a622fa5737f96ae0e3e15b16c29987
MrYakup <yakup.tk35@gmail.com>
2023-03-29 17:12:27 +0300

    first commit

---
+++ data.js
@@ -0,0 +1,712 @@
+
+export const events = [
+  {
+    "id": 1,
+    "title": "Poker night",
+    "date": "2019-09-12",
+    "location_id": 1,
+    "user_id": 1
+  },
+  {
+    "id": 2,
+    "title": "In her own words: \"By the waythe cheetah is a crocodile.",
+    "date": "2019-08-01",
+    "location_id": 11,
+    "user_id": 6
+  },
+  {
+    "id": 3,
+    "title": "To be more specific, a prune is a communicative pear",
+    "date": "2019-11-05",
+    "location_id": 16,
+    "user_id": 13
+  },
+  {
+    "id": 4,
+    "title": "It's an undeniable fact, really; the first wonderful grape is, in its own way, a fig",
+    "date": "2019-08-10",
+    "location_id": 12,
+    "user_id": 9
+  },
+  {
+    "id": 5,
+    "title": "A hamster sees a cheetah as an instinctive persimmon",
+    "date": "2019-10-25",
+    "location_id": 7,
+    "user_id": 12
+  },
+  {
+    "id": 6,
+    "title": "This is not to discredit the idea that we can assume that any instance of a blueberry can be construed as a humorous banana",
+    "date": "2019-05-03",
+    "location_id": 8,
+    "user_id": 15
+  },
+  {
+    "id": 7,
+    "title": "The lime is a scorpion",
+    "date": "2019-10-27",
+    "location_id": 10,
+    "user_id": 10
+  },
+  {
+    "id": 8,
+    "title": "Waking to the buzz of the alarm clock, some honest kumquats are thought of simply as pears",
+    "date": "2019-05-28",
+    "location_id": 15,
+    "user_id": 9
+  },
+  {
+    "id": 9,
+    "title": "A cheetah is a unbiased duck",
+    "date": "2019-11-25",
+    "location_id": 10,
+    "user_id": 15
+  },
+  {
+    "id": 10,
+    "title": "After a long day at school and work, the grapefruit is a lemon",
+    "date": "2019-03-16",
+    "location_id": 25,
+    "user_id": 6
+  },
+  {
+    "id": 11,
+    "title": "Authors often misinterpret the hippopotamus as a self-disciplined orange, when in actuality it feels more like a joyous frog",
+    "date": "2019-09-12",
+    "location_id": 16,
+    "user_id": 17
+  },
+  {
+    "id": 12,
+    "title": "A courageous apple without bananas is truly a wolf of helpful currants",
+    "date": "2019-03-23",
+    "location_id": 16,
+    "user_id": 12
+  },
+  {
+    "id": 13,
+    "title": "Draped neatly on a hanger, some helpful scorpions are thought of simply as bees",
+    "date": "2019-11-02",
+    "location_id": 18,
+    "user_id": 9
+  },
+  {
+    "id": 14,
+    "title": "A hamster sees a cow as a broad-minded hamster",
+    "date": "2019-12-27",
+    "location_id": 11,
+    "user_id": 13
+  },
+  {
+    "id": 15,
+    "title": "The nice scorpion comes from a decorous apple",
+    "date": "2019-08-28",
+    "location_id": 14,
+    "user_id": 12
+  },
+  {
+    "id": 16,
+    "title": "Owls are fabulous birds",
+    "date": "2019-08-01",
+    "location_id": 9,
+    "user_id": 14
+  },
+  {
+    "id": 17,
+    "title": "Few can name a tough duck that isn't a fearless horse",
+    "date": "2019-12-26",
+    "location_id": 12,
+    "user_id": 13
+  },
+  {
+    "id": 18,
+    "title": "A horse sees a kitten as a considerate currant",
+    "date": "2019-09-08",
+    "location_id": 7,
+    "user_id": 15
+  },
+  {
+    "id": 19,
+    "title": "If this was somewhat unclear, a fine crocodile without giraffes is truly a bear of energetic kittens",
+    "date": "2019-09-13",
+    "location_id": 9,
+    "user_id": 16
+  },
+  {
+    "id": 20,
+    "title": "They were lost without the modern duck that composed their watermelon",
+    "date": "2019-07-30",
+    "location_id": 9,
+    "user_id": 19
+  },
+  {
+    "id": 21,
+    "title": "As far as he is concerned, before turtles, lions were only giraffes",
+    "date": "2019-07-27",
+    "location_id": 7,
+    "user_id": 16
+  },
+  {
+    "id": 22,
+    "title": "In his own words: \"The first funny cheetah is, in its own way, a wolf.",
+    "date": "2019-04-26",
+    "location_id": 14,
+    "user_id": 15
+  },
+  {
+    "id": 23,
+    "title": "A thrifty grapefruit without sharks is truly a eagle of charming persimmons",
+    "date": "2019-03-28",
+    "location_id": 20,
+    "user_id": 8
+  },
+  {
+    "id": 24,
+    "title": "Gym",
+    "date": "2019-12-29",
+    "location_id": 25,
+    "user_id": 25
+  },
+  {
+    "id": 25,
+    "title": "Night hike and sleep",
+    "date": "2019-03-01",
+    "location_id": 12,
+    "user_id": 12
+  }
+];
+
+export const locations = [
+  {
+    "id": 1,
+    "name": "Lake Casandra",
+    "lat": -67.6213,
+    "lng": -40.0738
+  },
+  {
+    "id": 2,
+    "name": "East Enidville",
+    "lat": 56.9734,
+    "lng": -173.2386
+  },
+  {
+    "id": 3,
+    "name": "New Reuben",
+    "lat": 60.1949,
+    "lng": -130.7592
+  },
+  {
+    "id": 4,
+    "name": "Prosaccoborough",
+    "lat": -53.5301,
+    "lng": -147.5408
+  },
+  {
+    "id": 5,
+    "name": "Kennedyberg",
+    "lat": 54.4701,
+    "lng": 100.8425
+  },
+  {
+    "id": 6,
+    "name": "West Kaleigh",
+    "lat": 26.7929,
+    "lng": -69.9045
+  },
+  {
+    "id": 7,
+    "name": "Celestinehaven",
+    "lat": -34.564,
+    "lng": -154.2135
+  },
+  {
+    "id": 8,
+    "name": "South Mollymouth",
+    "lat": -48.1722,
+    "lng": 174.034
+  },
+  {
+    "id": 9,
+    "name": "Port Dena",
+    "lat": -47.7553,
+    "lng": -165.937
+  },
+  {
+    "id": 10,
+    "name": "Kubberg",
+    "lat": -66.7122,
+    "lng": -44.6965
+  },
+  {
+    "id": 11,
+    "name": "Filibertoland",
+    "lat": 74.0422,
+    "lng": -156.281
+  },
+  {
+    "id": 12,
+    "name": "Jeremieton",
+    "lat": -63.6556,
+    "lng": 116.3086
+  },
+  {
+    "id": 13,
+    "name": "Jarvischester",
+    "lat": 77.0237,
+    "lng": 92.867
+  },
+  {
+    "id": 14,
+    "name": "Hammesborough",
+    "lat": 12.2276,
+    "lng": 133.8686
+  },
+  {
+    "id": 15,
+    "name": "Rosefurt",
+    "lat": -75.1548,
+    "lng": 21.0084
+  },
+  {
+    "id": 16,
+    "name": "Herminioville",
+    "lat": -79.7145,
+    "lng": 32.5967
+  },
+  {
+    "id": 17,
+    "name": "Kathlynmouth",
+    "lat": -72.6088,
+    "lng": -156.6885
+  },
+  {
+    "id": 18,
+    "name": "Laruefurt",
+    "lat": -12.787,
+    "lng": -35.731
+  },
+  {
+    "id": 19,
+    "name": "Port Jeanie",
+    "lat": 51.3435,
+    "lng": 137.4496
+  },
+  {
+    "id": 20,
+    "name": "Kerlukeshire",
+    "lat": -29.2035,
+    "lng": -42.0214
+  },
+  {
+    "id": 21,
+    "name": "East Jeanie",
+    "lat": -86.7725,
+    "lng": 26.6931
+  },
+  {
+    "id": 22,
+    "name": "North Halliemouth",
+    "lat": -59.3469,
+    "lng": -37.1787
+  },
+  {
+    "id": 23,
+    "name": "Funkside",
+    "lat": -14.065,
+    "lng": 168.1448
+  },
+  {
+    "id": 24,
+    "name": "Queenchester",
+    "lat": 55.2666,
+    "lng": -11.0313
+  },
+  {
+    "id": 25,
+    "name": "Wunschfurt",
+    "lat": -44.9507,
+    "lng": -170.9158
+  }
+];
+
+export const users = [
+  {
+    "id": 1,
+    "username": "Strawberry6",
+    "email": "strawberry6@gmail.com"
+  },
+  {
+    "id": 2,
+    "username": "Apple3",
+    "email": "apple3@gmail.com"
+  },
+  {
+    "id": 3,
+    "username": "Currant5",
+    "email": "currant5@gmail.com"
+  },
+  {
+    "id": 4,
+    "username": "Hamster4",
+    "email": "hamster4@gmail.com"
+  },
+  {
+    "id": 5,
+    "username": "Melon5",
+    "email": "melon5@gmail.com"
+  },
+  {
+    "id": 6,
+    "username": "Kitten7",
+    "email": "kitten7@gmail.com"
+  },
+  {
+    "id": 7,
+    "username": "Pineapple8",
+    "email": "pineapple8@gmail.com"
+  },
+  {
+    "id": 8,
+    "username": "Wolf9",
+    "email": "wolf9@gmail.com"
+  },
+  {
+    "id": 9,
+    "username": "Rat2",
+    "email": "rat2@gmail.com"
+  },
+  {
+    "id": 10,
+    "username": "Hippopotamus8",
+    "email": "hippopotamus8@gmail.com"
+  },
+  {
+    "id": 11,
+    "username": "Tangerine6",
+    "email": "tangerine6@gmail.com"
+  },
+  {
+    "id": 12,
+    "username": "Lemon1",
+    "email": "lemon1@gmail.com"
+  },
+  {
+    "id": 13,
+    "username": "Grape8",
+    "email": "grape8@gmail.com"
+  },
+  {
+    "id": 14,
+    "username": "Alligator4",
+    "email": "alligator4@gmail.com"
+  },
+  {
+    "id": 15,
+    "username": "Seal2",
+    "email": "seal2@gmail.com"
+  },
+  {
+    "id": 16,
+    "username": "Snake9",
+    "email": "snake9@gmail.com"
+  },
+  {
+    "id": 17,
+    "username": "Eagle1",
+    "email": "eagle1@gmail.com"
+  },
+  {
+    "id": 18,
+    "username": "Currant4",
+    "email": "currant4@gmail.com"
+  },
+  {
+    "id": 19,
+    "username": "Frog9",
+    "email": "frog9@gmail.com"
+  },
+  {
+    "id": 20,
+    "username": "Snail9",
+    "email": "snail9@gmail.com"
+  },
+  {
+    "id": 21,
+    "username": "Spider9",
+    "email": "spider9@gmail.com"
+  },
+  {
+    "id": 22,
+    "username": "Ant1",
+    "email": "ant1@gmail.com"
+  },
+  {
+    "id": 23,
+    "username": "Currant4",
+    "email": "currant4@gmail.com"
+  },
+  {
+    "id": 24,
+    "username": "Watermelon3",
+    "email": "watermelon3@gmail.com"
+  },
+  {
+    "id": 25,
+    "username": "Fig2",
+    "email": "fig2@gmail.com"
+  }
+];
+
+export const participants = [
+  {
+    "id": 1,
+    "username":"participant_1",
+    "event_id": 1
+  },
+  {
+    "id": 2,
+    "username":"participant_2",
+    "event_id": 8
+  },
+  {
+    "id": 3,
+    "username":"participant_3",
+    "event_id": 4
+  },
+  {
+    "id": 4,
+    "username":"participant_4",
+    "event_id": 15
+  },
+  {
+    "id": 5,
+    "username":"participant_5",
+    "event_id": 17
+  },
+  {
+    "id": 6,
+    "username":"participant_6",
+    "event_id": 16
+  },
+  {
+    "id": 7,
+    "username":"participant_7",
+    "event_id": 10
+  },
+  {
+    "id": 8,
+    "username":"participant_8",
+    "event_id": 17
+  },
+  {
+    "id": 9,
+    "username":"participant_9",
+    "event_id": 7
+  },
+  {
+    "id": 10,
+    "username":"participant_10",
+    "event_id": 18
+  },
+  {
+    "id": 11,
+    "username":"participant_11",
+    "event_id": 18
+  },
+  {
+    "id": 12,
+    "username":"participant_12",
+    "event_id": 14
+  },
+  {
+    "id": 13,
+    "username":"participant_13",
+    "event_id": 16
+  },
+  {
+    "id": 14,
+    "username":"participant_14",
+    "event_id": 11
+  },
+  {
+    "id": 15,
+    "username":"participant_15",
+    "event_id": 13
+  },
+  {
+    "id": 16,
+    "username":"participant_16",
+    "event_id": 20
+  },
+  {
+    "id": 17,
+    "username":"participant_17",
+    "event_id": 11
+  },
+  {
+    "id": 18,
+    "username":"participant_18",
+    "event_id": 14
+  },
+  {
+    "id": 19,
+    "username":"participant_19",
+    "event_id": 15
+  },
+  {
+    "id": 20,
+    "username":"participant_20",
+    "event_id": 14
+  },
+  {
+    "id": 21,
+    "username":"participant_21",
+    "event_id": 12
+  },
+  {
+    "id": 22,
+    "username":"participant_22",
+    "event_id": 10
+  },
+  {
+    "id": 23,
+    "username":"participant_23",
+    "event_id": 12
+  },
+  {
+    "id": 24,
+    "username":"participant_24",
+    "event_id": 15
+  },
+  {
+    "id": 25,
+    "username":"participant_25",
+    "event_id": 13
+  },
+  {
+    "id": 26,
+    "username":"participant_26",
+    "event_id": 14
+  },
+  {
+    "id": 27,
+    "username":"participant_27",
+    "event_id": 15
+  },
+  {
+    "id": 28,
+    "username":"participant_28",
+    "event_id": 14
+  },
+  {
+    "id": 29,
+    "username":"participant_29",
+    "event_id": 11
+  },
+  {
+    "id": 30,
+    "username":"participant_30",
+    "event_id": 18
+  },
+  {
+    "id": 31,
+    "username":"participant_31",
+    "event_id": 8
+  },
+  {
+    "id": 32,
+    "username":"participant_32",
+    "event_id": 5
+  },
+  {
+    "id": 33,
+    "username":"participant_33",
+    "event_id": 14
+  },
+  {
+    "id": 34,
+    "username":"participant_34",
+    "event_id": 12
+  },
+  {
+    "id": 35,
+    "username":"participant_35",
+    "event_id": 17
+  },
+  {
+    "id": 36,
+    "username":"participant_36",
+    "event_id": 12
+  },
+  {
+    "id": 37,
+    "username":"participant_37",
+    "event_id": 11
+  },
+  {
+    "id": 38,
+    "username":"participant_38",
+    "event_id": 14
+  },
+  {
+    "id": 39,
+    "username":"participant_39",
+    "event_id": 12
+  },
+  {
+    "id": 40,
+    "username":"participant_40",
+    "event_id": 12
+  },
+  {
+    "id": 41,
+    "username":"participant_41",
+    "event_id": 6
+  },
+  {
+    "id": 42,
+    "username":"participant_42",
+    "event_id": 8
+  },
+  {
+    "id": 43,
+    "username":"participant_43",
+    "event_id": 12
+  },
+  {
+    "id": 44,
+    "username":"participant_44",
+    "event_id": 12
+  },
+  {
+    "id": 45,
+    "username":"participant_45",
+    "event_id": 5
+  },
+  {
+    "id": 46,
+    "username":"participant_46",
+    "event_id": 12
+  },
+  {
+    "id": 47,
+    "username":"participant_47",
+    "event_id": 16
+  },
+  {
+    "id": 48,
+    "username":"participant_48",
+    "event_id": 13
+  },
+  {
+    "id": 49,
+    "username":"participant_49",
+    "event_id": 25
+  },
+  {
+    "id": 50,
+    "username":"participant_50",
+    "event_id": 12
+  }
+];
